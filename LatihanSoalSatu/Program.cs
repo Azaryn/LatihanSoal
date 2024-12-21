7afc039889909e0e0fbc553905fa2f24bda57af1
Rafi Hadianto A <hadianto.rafi@gmail.com>
2024-12-21 19:17:37 +0700

    first commit

---
+++ LatihanSoalSatu/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanSoalSatu
+{ 
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa pRafi = new Mahasiswa("Rafi Hadianto", "Laki-laki", "Jember", 2006);
+            Sepeda kRafi = new Sepeda("Yamaha", 2022, "Fazzio");
+            Console.WriteLine("MAHASISWA FASILKOM");
+            pRafi.TampilkanInfo();
+            Console.WriteLine(kRafi.info());
+            Console.WriteLine(kRafi.mesin());
+
+            Console.WriteLine(" ");
+
+            Dosen pTunggul = new Dosen("Tunggul Abdul", "Laki-laki", "Banyuwangi", 2058);
+            Sedan kTunggul = new Sedan("BMW", 2024, "Z4 Roaster");
+            Console.WriteLine("DOSEN FASILKOM");
+            pTunggul.TampilkanInfo();
+            Console.WriteLine(kTunggul.info());
+            Console.WriteLine(kTunggul.mesin());
+
+            Console.WriteLine(" ");
+
+            Tendik pLubia = new Tendik("Lubia Fahri", "Laki laki", "Madiun", 2090);
+            Truk kLubia = new Truk("Pikap", 2025, "Suzuki New Carry");
+            Console.WriteLine("Tendik FASILKOM");
+            pLubia.TampilkanInfo();
+            Console.WriteLine(kLubia.info());
+            Console.WriteLine(kLubia.mesin());
+
+            Console.ReadLine();
+
+        }
+    }
+    public class Person //class
+    {
+        public string Nama;
+        public string JenisKelamin;
+        public string Alamat;
+
+        public Person(string nama, string jenisKelamin, string alamat)
+        {
+            Nama = nama;
+            JenisKelamin = jenisKelamin;
+            Alamat = alamat;
+        }
+
+    }
+    public class Mahasiswa : Person //subclass
+    {
+        public int NIM;
+
+        public Mahasiswa(string nama, string jenisKelamin, string alamat, int nim)
+            : base(nama,jenisKelamin,alamat)
+        {
+            NIM = nim;
+        }
+        public void TampilkanInfo()
+        {
+            Console.WriteLine($"Nama: {Nama}");
+            Console.WriteLine($"Jenis Kelamin: {JenisKelamin}");
+            Console.WriteLine($"Alamat: {Alamat}");
+            Console.WriteLine($"NIM: {NIM}");
+        }
+    }
+    public class Dosen : Person //subclass
+    {
+        public int NID;
+
+        public Dosen(string nama, string jenisKelamin, string alamat, int nid)
+            : base(nama, jenisKelamin, alamat)
+        {
+            NID = nid;
+        }
+        public void TampilkanInfo()
+        {
+            Console.WriteLine($"Nama: {Nama}");
+            Console.WriteLine($"Jenis Kelamin: {JenisKelamin}");
+            Console.WriteLine($"Alamat: {Alamat}");
+            Console.WriteLine($"NID: {NID}");
+        }
+    }
+    public class Tendik : Person //subclass
+    {
+        public int NIA;
+
+        public Tendik(string nama, string jenisKelamin, string alamat, int nia) 
+            : base(nama, jenisKelamin, alamat)
+        {
+            NIA = nia;
+        }
+        public void TampilkanInfo()
+        {
+            Console.WriteLine($"Nama: {Nama}");
+            Console.WriteLine($"Jenis Kelamin: {JenisKelamin}");
+            Console.WriteLine($"Alamat: {Alamat}");
+            Console.WriteLine($"NIA: {NIA}");
+        }
+    }
+
+    public class Kendaraan //class
+    {
+        public string JenisKendaraan;
+        public int Tahun;
+
+        public Kendaraan(string jenisKendaraan, int tahun)
+        {
+            JenisKendaraan = jenisKendaraan;
+            Tahun = tahun;
+        }
+        public virtual string info()
+        {
+            return $"Kendaraan: {JenisKendaraan}\nTahun:{Tahun}";
+        }
+        public virtual string mesin()
+        {
+            return "kapasitas mesin ini standar";
+        }
+
+    }
+    public class Sedan : Kendaraan //subclass
+    {
+        public string Series;
+        public Sedan(string jenisKendaraan, int tahun, string series) 
+            : base(jenisKendaraan, tahun)
+        {
+            Series = series;
+        }
+        public override string info()
+        {
+            return $"Kendaraan: {JenisKendaraan}\nTahun: {Tahun}\nSeries: {Series}";
+        }
+        public override string mesin()
+        {
+            return "Kapasitas mesin mobil sekitar 50–1500 cc";
+        }
+    }
+    public class Truk : Kendaraan //subclass
+    {
+        public string Series;
+        public Truk(string jenisKendaraan, int tahun, string series) 
+            : base(jenisKendaraan, tahun)
+        {
+            Series = series;
+        }
+        public override string info()
+        {
+            return $"Kendaraan: {JenisKendaraan}\nTahun: {Tahun}\nSeries: {Series}";
+        }
+        public override string mesin()
+        {
+            return "Kapasitas mesin Truk sekitar 3.908-7.790cc";
+        }
+    }
+    public class Sepeda : Kendaraan //subclass
+    {
+        public string Series;
+        public Sepeda(string jenisKendaraan, int tahun, string series)
+            : base(jenisKendaraan, tahun)
+        {
+            Series = series;
+        }
+        public override string info()
+        {
+            return $"Kendaraan: {JenisKendaraan}\nTahun: {Tahun}\nSeries: {Series}";
+        }
+        public override string mesin()
+        {
+            return "Kapasitas mesin ini 100-600cc";
+        }
+    }
+}
---
+++ LatihanSoalSatu/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LatihanSoalSatu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LatihanSoalSatu")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("73c2d8f0-20c3-4d37-bd46-c671d92926bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
